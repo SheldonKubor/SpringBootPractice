16e9d7e1c9011eeff29aa922ebb43a0f183d40a7
Constantine <mjh123877@163.com>
2017-11-09 10:17:12 +0800

    集成swagger

--- src/main/java/com/mjh/controller/UserController.java
+++ src/main/java/com/mjh/controller/UserController.java
@@ -2,6 +2,7 @@ package com.mjh.controller;
 
 import com.mjh.dao.UserRepository;
 import com.mjh.model.User;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,18 +22,21 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @ApiOperation(value = "获取所有用户")
     @GetMapping(value = "/all")
     public List<User> getAllUsers(){
         List<User> userList = userRepository.findAll();
         return userList;
     }
 
+    @ApiOperation(value = "添加一个用户",notes = "只需传入username和password")
     @PostMapping(value = "/add")
     public User addUser(User user){
         User user1 = userRepository.save(user);
         return user1;
     }
 
+    @ApiOperation(value = "根据id获取用户")
     @GetMapping(value = "/{id}")
     public User getUserByName(@PathVariable Integer id){
         User user = userRepository.findOne(id);
